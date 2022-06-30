3bb8293783fec3a7c7e70b7179219fc442cbbd1f
Emmanuel Unche <emyduke@users.noreply.github.com>
2022-06-30 04:56:33 +0100

    Globally responsive

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
-    // $(window).scroll(function(){
-    //     console.log($(window).scrollTop())
-    //     if($(window).scrollTop() > 50){
-    //         $(".nav-link").css({"color":"black"});   
-    //         $(".offcanvas-body").find($(".btn")).removeClass("btn-outline-light");
-    //         $(".offcanvas-body").find($(".btn")).addClass("btn-outline-dark");
-            
-    //     }
-    //     else{
-    //         $(".nav-link").css({"color":"white"});
-    //         $(".offcanvas-body").find($(".btn")).removeClass("btn-outline-dark");
-    //         $(".offcanvas-body").find($(".btn")).addClass("btn-outline-light");
-    //     }
+$(document).ready(function () {
+    $(window).scroll(function(){
+        console.log($(window).scrollTop())
+        if($(window).scrollTop() > 50){
+            $(".navbar").addClass("bg-nav");
+            // $(".nav-link").css({"color":"black"});   
+            // $(".offcanvas-body").find($(".btn")).removeClass("btn-outline-light");
+            // $(".offcanvas-body").find($(".btn")).addClass("btn-outline-dark");
 
-    // })
+        }
+        else{
+            $(".navbar").removeClass("bg-nav");
+            // $(".nav-link").css({"color":"white"});
+            // $(".offcanvas-body").find($(".btn")).removeClass("btn-outline-dark");
+            // $(".offcanvas-body").find($(".btn")).addClass("btn-outline-light");
+        }
+
+    })
 
 
     $('.testimonial-slider').slick({
@@ -21,19 +23,25 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 3
+            {
+                breakpoint: 1050,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    // arrows: false,
+                    slidesToShow: 1
+                }
             }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              // arrows: false,
-              slidesToShow: 1
-            }
-          }
         ]
-      });
+    });
 })
